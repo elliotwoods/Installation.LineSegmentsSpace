b630d6425ca5154919388dff4fe35f9403ade913
Mimi Son <mimison@Mimis-MacBook-Pro.local>
2017-12-06 19:51:37 +0000

    merge

--- mapLinesRemoteLaptop/src/pch.h
+++ mapLinesRemoteLaptop/src/pch.h
@@ -2,12 +2,7 @@
 
 #include "ofMain.h"
 #include "ofxCvGui.h"
-<<<<<<< HEAD
 
 #ifdef TARGET_OSX
 #include "json.hpp"
 #endif
-=======
-#include "ofxRay.h"
-#include "json.hpp"
->>>>>>> e19867fc19d2c3c9e7653645c898ffdf296b1a1c
