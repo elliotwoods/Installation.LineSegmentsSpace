8ac6594c66586bb0edebdee5ebd91a6bcad63150
Elliot Woods <elliot.woods@gmail.com>
2018-01-12 22:32:31 +0000

    derby recalibration

---
+++ 36. Production JSON Mapped/subpatches/Subpatches/Out 0 - Renderers/HomographyAdjusters/dx11/DrawInViewport.fx
@@ -0,0 +1,66 @@
+struct vsInput
+{
+	uint ii : SV_InstanceID; //Object instance ID
+    float4 posObject : POSITION;
+    float2 uv: TEXCOORD0;
+};
+
+int ProjectorIndex;
+
+Texture2D inputTexture <string uiname="Texture";>;
+
+SamplerState Sampler <string uiname="Sampler State";>
+{
+    Filter = MIN_MAG_MIP_POINT;
+    AddressU = Clamp;
+    AddressV = Clamp;
+};
+
+struct psInput
+{
+    float4 posScreen : SV_Position;
+    float2 uv: TEXCOORD0;
+};
+
+cbuffer cbPerDraw : register(b0)
+{
+	float4x4 tVP : LAYERVIEWPROJECTION;
+	int ViewportIndex : VIEWPORTINDEX;
+};
+
+cbuffer cbPerObj : register( b1 )
+{
+	float4x4 tW : WORLD;
+	float4 cAmb <bool color=true;String uiname="Color";> = { 1.0f,1.0f,1.0f,1.0f };
+};
+
+psInput VS(vsInput input)
+{
+	psInput output;
+	
+	output.posScreen = mul(input.posObject, mul(tW,tVP));
+
+	output.uv = input.uv;
+	
+	if(ProjectorIndex != ViewportIndex) {
+		output.posScreen = 0;
+	}
+	
+	return output;
+}
+
+float4 PS(psInput input): SV_Target
+{
+    float4 col = inputTexture.Sample(Sampler,input.uv.xy);;
+    return col * cAmb;
+}
+
+
+technique11 DrawInViewport
+{
+	pass P0
+	{
+		SetVertexShader( CompileShader( vs_4_0, VS() ) );
+		SetPixelShader( CompileShader( ps_4_0, PS() ) );
+	}
+}
---
+++ 36. Production JSON Mapped/subpatches/Subpatches/Out 0 - Renderers/HomographyAdjusters/plugins/2DFindCorners/C2DFindCornersNode.cs
@@ -0,0 +1,78 @@
+#region usings
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+
+using VVVV.PluginInterfaces.V1;
+using VVVV.PluginInterfaces.V2;
+using VVVV.Utils.VColor;
+using VVVV.Utils.VMath;
+
+using VVVV.Core.Logging;
+#endregion usings
+
+namespace VVVV.Nodes
+{
+	#region PluginInfo
+	[PluginInfo(Name = "FindCorners", Category = "2D", Help = "Basic template with one value in/out", Tags = "c#")]
+	#endregion PluginInfo
+	public class C2DFindCornersNode : IPluginEvaluate
+	{
+		#region fields & pins
+		[Input("Projector Index", IsSingle=true)]
+		public IDiffSpread<int> FInProjectorIndex;
+		
+		[Input("Lines")]
+		public IDiffSpread<ISpread<Vector2D>> FInLines;
+
+		[Input("iProjector")]
+		public IDiffSpread<int> FInIProjector;
+		
+		[Input("Targets")]
+		public IDiffSpread<Vector2D> FInTargets;
+		
+		[Output("Output")]
+		public ISpread<Vector2D> FOutput;
+
+		[Import()]
+		public ILogger FLogger;
+		#endregion fields & pins
+
+		//called when data for any output pin is requested
+		public void Evaluate(int SpreadMax)
+		{
+			if(FInLines.IsChanged
+			|| FInIProjector.IsChanged
+			|| FInProjectorIndex.IsChanged
+			|| FInTargets.IsChanged) {
+				//setup distances and output
+				var distanceToTargets = new List<double>();
+				foreach(var target in FInTargets) {
+					distanceToTargets.Add(double.MaxValue);
+				}
+				FOutput.SliceCount = FInTargets.SliceCount;
+				
+				//check all corners
+				for(int i=0; i<FInLines.SliceCount; i++) {
+					if(FInIProjector[i] != FInProjectorIndex[i]) {
+						//not a matching line
+						continue;
+					}
+					
+					var line = FInLines[i];
+					foreach(var corner in line) {
+						for(int targetIndex = 0; targetIndex < FInTargets.SliceCount; targetIndex++) {
+							var target = FInTargets[targetIndex];
+							var distanceToTarget = (target - corner).LengthSquared;
+							if(distanceToTarget < distanceToTargets[targetIndex]) {
+								distanceToTargets[targetIndex] = distanceToTarget;
+								FOutput[targetIndex] = corner;
+							}
+						}
+					}
+				}
+			}
+			//FLogger.Log(LogType.Debug, "hi tty!");
+		}
+	}
+}
